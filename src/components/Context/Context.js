7a5416f9fcaf8dd1fa991cc652dcbcb162e5da96
Valdemiro Viegas <valdemiroviegas@yahoo.com>
2022-03-21 10:50:54 +0000

    categories

---
+++ src/components/Context/Context.js
@@ -0,0 +1,35 @@
+import React, { createContext, useCallback, useState } from "react";
+import axios from 'axios' ;
+export const myContext = createContext();
+
+export const AppContext = ({ children }) => {
+    const [meals, setMeals] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [randomMeal, setRandomMeal] = useState([]);
+
+    const fetchHomePageMeals = useCallback((searchTerm)=>{
+        axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+        .then(res=>{
+            setMeals(res.data.meals);
+            console.log(res.data.meals);
+        })
+    },[])
+
+    const fetchCategories = useCallback(()=>{
+        axios.get(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+        .then(res=>{
+            console.log(res.data.categories);
+            setCategories(res.data.categories);
+        });
+    },[]);
+
+    
+
+    return <myContext.Provider 
+                value={{fetchHomePageMeals,meals, fetchCategories, categories}}>
+                    {children}
+            </myContext.Provider>;
+}
+
+
+
---
+++ src/components/RandomMeal/RandomMeal.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './RandomMeal.scss';
+
+const RandomMeal = () => {
+
+    
+    return ( 
+        <div className="random">
+                
+        </div>
+     );
+}
+ 
+export default RandomMeal;
